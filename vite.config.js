509fca2d3bb442898c2f924bb8e4d9796c886818
Stratos915 <stratosdiakatos@yahoo.it>
2025-11-23 13:31:29 +0100

    Fix: base path per deploy Netlify

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // IMPORTANTE: nome del repository dopo lo slash
-  base: '/Speleo/',
+  // PER NETLIFY: niente sotto-cartella, quindi:
+  base: '/',
 })
